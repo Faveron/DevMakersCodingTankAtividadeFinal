5241b51910089c16bac8b8a16ce01ff940637582
Wesley Faveron <w.faveron@aluno.ifsp.edu.br>
2022-10-25 23:30:59 -0300

    Código básico para pegar um número inteiro sem dar exceção

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        while (true)
+        {
+            int n = getNumber();
+            System.out.println("O valor digitado é: " + n);
+        }
+    }
+
+    public static int getNumber() {
+        Scanner entrada = new Scanner(System.in);
+        String numero;
+        do {
+            System.out.print("Digite o valor: ");
+            numero = entrada.nextLine();
+        } while (!eInteriro(numero));
+
+        return Integer.parseInt(numero);
+    }
+    public static boolean eInteriro(String texto) {
+        try {
+            Integer.parseInt(texto);
+            return true;
+        } catch (Exception e) {
+            System.out.println("O número digitado não é inteiro, tente novamente");
+            return false;
+        }
     }
 }
